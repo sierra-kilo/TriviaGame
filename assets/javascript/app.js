b563f8c8dd614a9976d2d3241ca64acae3486c9e
Sierra Kilo <sierrakilo@Sierras-MacBook-Pro.local>
2017-08-17 12:44:00 -0700

    started html, testing app.js worked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,36 @@
 // global variable assignments
-
 var correctAnswers = null;
 var incorrectAnswers = null;
 var timeRemaining = 10;
 var questionList = [];
+var myTimer;
 
 function subOne() {
     timeRemaining--;
     console.log(timeRemaining)
     if (timeRemaining === 0) {
-        clearInterval(myVar);
-    }    
+        clearInterval(myTimer);
+    }
 }
 
 function countdown() {
-    var myVar = setInterval(subOne, 1000);
-
+    myTimer = setInterval(subOne, 1000);
 }
+
+var question0 = {
+    question: 'What is your name?',
+
+    choice0: 'Jon',
+    choice1: 'Jamie',
+    choice2: 'Shant',
+
+    correct: 'Shant',
+
+    logFunction: function () {
+        console.log(question0.question, question0.choice0, this.correct, this);
+        }
+    };
+
+countdown();
+console.log(question0.correct);
+// question0.logFunction();
