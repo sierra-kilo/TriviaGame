efee20ed330c20fabeadbf63021844f193e91f06
Sierra Kilo <sierrakilo@Sierras-MacBook-Pro.local>
2017-08-18 21:53:51 -0700

    made new index file, made new app3.js file. hides divs at start, start button starts timer and shows quesitonsaire

---
+++ assets/javascript/app3.js
@@ -0,0 +1,51 @@
+// variables we might needd
+var timeRemaining = 20+1;
+var correctAnswers;
+var incorrectAnswers;
+var questionList = [];
+var choice;
+var correct;
+var gameStarted = false;
+var myTimer;
+
+// what we need to do
+
+// load page
+// only load instructions
+if(!gameStarted) {
+    $(function () {
+        $('.questionaire').hide();
+        $('.time-display').hide();
+        $('.results-display').hide();
+    });
+}
+
+// click start button
+$('.btn[name=start]').click(function() {
+    $('.instructions').hide();
+    // show questions and response options
+    $('.questionaire').show();
+    // show and start countdown
+    $('.time-display').show();
+    myTimer = setInterval(countdown, 100)
+    countdown();
+});
+
+
+// countdown functions
+function countdown() {
+    timeRemaining--;
+    $('.countdown').text(timeRemaining);
+    if (timeRemaining <= 0) {
+        clearInterval(myTimer);
+    }
+}
+
+// dynamically create questions and choices and submit button
+
+
+
+// wait for end of timer or submit button
+// remove question
+// grade
+// show results results
---
+++ assets/javascript/questionList.js
@@ -0,0 +1,20 @@
+var questions = [
+    {
+        question: 'Is Harry a muggle?',
+        choices: [
+            'True',
+            'False',
+        ],
+        correct: 1,
+    },
+    {
+        question: 'Who is a slytherin?',
+        choices: [
+            'Harry',
+            'Luna',
+            'Ron',
+            'Malfoy',
+        ],
+        correct: 3,
+    }
+]
